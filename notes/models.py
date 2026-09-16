30beae289c67ff379847eefee07983159a1067e5
sushant-agi <sushant2508.tech@gmail.com>
2026-09-16 06:04:47 +0530

    Feat: Add Note Model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
