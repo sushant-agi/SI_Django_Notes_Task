d03b388363a5c44481e95b1cd8a8c7e51b5a9ea7
sushant-agi <sushant2508.tech@gmail.com>
2026-09-16 07:37:43 +0530

    Feat: Add User Registration

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def home(request):
-    return HttpResponse("Welcome to my notes app!")
+    return render(request, 'notes/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        return redirect('home')
+
+    return render(request, 'notes/register.html')
