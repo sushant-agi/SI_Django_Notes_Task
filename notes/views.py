b45ec6ab2a1cf4fe56b4178aa8ab6459b4b8d494
sushant-agi <sushant2508.tech@gmail.com>
2026-09-16 08:12:39 +0530

    Feat: Add user login

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, 'notes/home.html')
@@ -17,3 +18,25 @@ def register(request):
         return redirect('home')
 
     return render(request, 'notes/register.html')
+
+def login_view(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=authenticate(
+            request,
+            username=username, 
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        return render(
+            request,
+            'notes/login.html',
+            {'error': 'Invalid username or password'}
+        )
+    return render(request, 'notes/login.html')
