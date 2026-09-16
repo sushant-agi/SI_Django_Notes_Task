76e7e75465f2daaa5fdccc5d734b2cbe49a60046
sushant-agi <sushant2508.tech@gmail.com>
2026-09-16 08:37:59 +0530

    Feat: Protect pages with authentication

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render(request, 'notes/home.html')
 
